879e1fddc38092d6d2a2b8abbdecfa452288a77c
aljt <alvinljt@gmail.com>
2020-11-23 00:21:28 +0800

    deleted comments and added pg configuration code

--- index.js
+++ index.js
@@ -6,25 +6,22 @@ import express from 'express';
 // get Pool function from the library
 const { Pool } = pg;
 
-// // set the way we will connect (configuration) to the server
-// const pgConnectionConfigs = {
-//   user: 'aljt',
-//   host: 'localhost',
-//   database: 'aljt',
+// set the way we will connect (configuration) to the server
+// if we do not create this object (pgConnectionConfigs),
+// then postgresql will just use the db with the same name as
+// that of the user who is running the node command
+const pgConnectionConfigs = {
+  user: 'aljt',
+  host: 'localhost',
+  database: 'aljt',
 
-//   port: 5432, // Postgres server always runs on this port
-// };
-
-// // create a new instance of the client
-// const client = new Client(pgConnectionConfigs);
-
-// // make the connection to the server
-// client.connect();
+  port: 5432, // Postgres server always runs on this port
+};
 
 // create a new instance of Pool,
 // the fn that manages the database connections
 // that Express.js is making and appropriately end those connections
-const pool = new Pool();
+const pool = new Pool(pgConnectionConfigs);
 
 // create an instance of express
 const app = express();
@@ -57,47 +54,3 @@ app.get('/', (request, response) => {
 });
 
 app.listen(PORT);
-
-// to store the SQL query so we can pass it as a parameter to client.query
-// let sqlQuery;
-
-// // create the query done callback
-// const whenQueryDone = (error, result) => {
-//   // this error is anything that goes wrong with the query
-//   if (error) {
-//     console.log('error', error);
-//   } else {
-//     // rows key has the data
-//     // shows the data of all the rows of the database
-//     // only when sqlQuery = 'SELECT * from meals'
-//     console.log('result.rows:', result.rows);
-//   }
-
-//   // close the connection
-//   client.end();
-// };
-
-// // get the arguments in the command line
-// // create the sql query
-// // run the query using the client
-// if (process.argv[2] === 'report') {
-//   // write the SQL query that displays all the rows in meals table
-//   sqlQuery = 'SELECT * from meals';
-
-//   // run the SQL query
-//   client.query(sqlQuery, whenQueryDone);
-// } else if (process.argv[2] === 'log') {
-//   // create vars for the meal information
-//   const type = process.argv[3];
-//   const description = process.argv[4];
-//   const amtOfAlcohol = process.argv[5];
-//   const wasHungryBe4Eating = process.argv[6];
-
-//   const values = [type, description, amtOfAlcohol, wasHungryBe4Eating];
-
-//   // write the SQL query that inserts a new meal entry
-//   sqlQuery = 'INSERT INTO meals(type, description, amount_of_alcohol, was_hungry_before_eating) VALUES ($1, $2, $3, $4)';
-
-//   // run the SQL query
-//   client.query(sqlQuery, values, whenQueryDone);
-// }
